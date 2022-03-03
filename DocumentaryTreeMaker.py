15a231193f09f8e9622a0ddd3906e59dc70dd702
Athe <atheicus@ezeion.com>
2022-03-03 23:12:58 +0000

    encountered error, quick fix

--- DocumentaryTreeMaker.py
+++ DocumentaryTreeMaker.py
@@ -3,11 +3,10 @@ import os, collections, pathlib
 from os.path import isfile
 
 #list full path to your workspace
-directory = r"C:\Users\G\OneDrive - ekdls\!Programs\Python\Stuff\SameStructureStyleDocumenting\config"
+directory = r""
 
 #list full path to your desired location to place the output
-output_directory = r"C:\Users\G\OneDrive - ekdls\!Programs\Python\Stuff\SameStructureStyleDocumenting\Testing"
-print(output_directory)
+output_directory = r""
 
 #extension type 
 extension_type = ".md" # use .md for github pages and other services that rely on .md files
@@ -15,26 +14,37 @@ extension_type = ".md" # use .md for github pages and other services that rely o
 ###############################################
 dq = collections.deque([directory])
 stoppable = False
+samename_n = 0
 
 def finderskeepers(dqt):
     output_current_directory = os.path.relpath(dqt, directory)
+    
     for object in os.listdir(dqt):
         item = os.path.join(current_path, object)
-        if os.path.isfile(item):
+        if os.path.isfile(item) and not object.startswith('.'):
             x = object.rsplit('.', 1)
             text_file = x[0] + extension_type
-            open(os.path.join(output_directory, output_current_directory,text_file), "x")
-        elif os.path.isdir(item):
+            try:
+                open(os.path.join(output_directory, output_current_directory,text_file), "x")
+            except FileExistsError:
+                samename_n += 1
+                text_file = x[0] + "(" + str(samename_n) + ")" + extension_type 
+                open(os.path.join(output_directory, output_current_directory, text_file), "x")
+            samename_n = 0
+
+        elif os.path.isdir(item) and not object.startswith('.'):
             d = os.path.join(output_directory, output_current_directory, object)
             os.mkdir(d)
             dq.append(item)
         else:
-            print("Unknown!")
+            print("Unknown! (It's probably a hidden file) and we're ignoring it because it wants to stay hidden: ", item)
         
 while stoppable != True and len(dq) != 0:
     current_path = dq.popleft()
     finderskeepers(current_path)
     stoppable == True
+
+
 
 print("Successful 🥺")
 
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
  ### For the purpose of creating a documention
-...that's structured the same as your codebase heh
+...that's structured the same as your codebase heh (only useful for an already-created and grown codebase)
 
 Used on Windows (11), but might work for other OS, also using python3.10
